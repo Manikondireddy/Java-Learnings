f85ac3681d4db72e43d4e8816593db3a8d98f186
manikondireddy <mani.kondireddy@fissionland.com>
2025-08-06 15:30:19 +0530

    adding good lines

---
+++ JavaExamples/JAVATASKS/Demo.java
@@ -0,0 +1,16 @@
+package JavaExamples.JAVATASKS;
+
+public class Demo {
+    static void add(int a)
+    {
+        System.out.println(a+a);
+    }
+    public static void main(String[]args)
+    {
+        int x=90;
+        add(x);
+    }
+
+    }
+    
+
---
+++ JavaExamples/JAVATASKS/Sample.java
@@ -0,0 +1,16 @@
+package JavaExamples.JAVATASKS;
+
+public class Sample {
+    int x=89;
+    static void disp(Sample s2)
+    {
+        System.out.println(s2.x);
+    }
+    public static void main(String[] args)
+     {
+        Sample s1= new Sample();
+        disp (s1);
+        
+    }
+    }
+
